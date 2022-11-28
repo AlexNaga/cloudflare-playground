c7fbb6fa11e0466ee7bbc42d50f555afe477b77a
Alex <alex.naga@hotmail.com>
2022-11-28 17:09:20 +0100

    feat: not google

--- not-google/src/index.ts
+++ not-google/src/index.ts
@@ -1,12 +1,40 @@
-addEventListener("fetch", function (event: FetchEvent) {
-  event.respondWith(handleRequest(event.request));
-});
-async function handleRequest(request) {
-  // Only GET requests work with this proxy.  if (request.method !== 'GET') return MethodNotAllowed(request);  return fetch(`https://example.com`);
+const HOSTNAME = "google.com";
+const NOT_GOOGLE_IMG_URL =
+  " https://www.searchenginewatch.com/wp-content/uploads/2018/10/notogoogle-1.png";
+
+class ImageHandler {
+  element(element: HTMLElement) {
+    element.setAttribute("src", NOT_GOOGLE_IMG_URL);
+    element.setAttribute("srcset", NOT_GOOGLE_IMG_URL);
+  }
 }
-function MethodNotAllowed(request) {
-  return new Response(`Method ${request.method} not allowed.`, {
-    status: 405,
-    headers: { Allow: "GET" },
-  });
+
+class ElementHandler {
+  element(element: HTMLElement) {
+    element.setInnerContent("🙅‍♀️ Not Google");
+  }
 }
+
+export default {
+  async fetch(
+    request: Request,
+    env: {},
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    url.hostname = HOSTNAME;
+    const fetchRequest: RequestInit<RequestInitCfProperties> = {
+      headers: request.headers,
+    };
+
+    const originalResponse = await fetch(url.toString(), fetchRequest);
+
+    const transformed = new HTMLRewriter()
+      .on("img", new ImageHandler())
+      .on("title", new ElementHandler())
+      .on("h1", new ElementHandler())
+      .transform(originalResponse);
+
+    return transformed;
+  },
+};
